46ada0e1e2c61bdeb28d9e59d1d6366f45c45d96
Domder-WEB <dominiktaras42@gmail.com>
2026-06-23 21:58:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const CREATOR_WEBHOOK = "TVŮJ_CREATOR_WEBHOOK";
-const EDITOR_WEBHOOK = "TVŮJ_EDITOR_WEBHOOK";
+const CREATOR_WEBHOOK = "CREATOR_WEBHOOK";
+const EDITOR_WEBHOOK = "EDITOR_WEBHOOK";
 
 function openCreator() {
   document.getElementById("creatorForm").classList.remove("hidden");
@@ -11,47 +11,33 @@ function openEditor() {
   document.getElementById("creatorForm").classList.add("hidden");
 }
 
-/* CREATOR */
 function sendCreator() {
   fetch(CREATOR_WEBHOOK, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({
       content:
-`🎥 CreatorLink – Creator Request
-────────────────
-👤 Name: ${c_name.value}
-📸 IG: ${c_ig.value}
-📱 Platform: ${c_platform.value}
-🎬 Style: ${c_style.value}
-💰 Budget: ${c_budget.value}
-💳 Payment: ${c_payment.value}
-📝 Notes: ${c_extra.value}
-────────────────`
+`🎥 CreatorLink Request
+👤 ${c_name.value}
+📸 ${c_ig.value}
+🎬 ${c_style.value}
+💰 ${c_budget.value}
+`
     })
   });
-
-  alert("Odesláno");
 }
 
-/* EDITOR */
 function sendEditor() {
   fetch(EDITOR_WEBHOOK, {
     method: "POST",
     headers: {"Content-Type": "application/json"},
     body: JSON.stringify({
       content:
-`✂️ CreatorLink – Editor Request
-────────────────
-👤 Name: ${e_name.value}
-📸 IG: ${e_ig.value}
-⚡ Skills: ${e_skills.value}
-💰 Price: ${e_price.value}
-💳 Payment: ${e_payment.value}
-📁 Portfolio: ${e_portfolio.value}
-────────────────`
+`✂️ EditorLink Request
+👤 ${e_name.value}
+⚡ ${e_skills.value}
+💰 ${e_price.value}
+`
     })
   });
-
-  alert("Odesláno");
 }
